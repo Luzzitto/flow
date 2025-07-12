5071f024274924bf75caa60972a8a26f527b09b8
Luzzitto Tupaz <36060968+Luzzitto@users.noreply.github.com>
2025-07-12 00:57:26 -0400

    Add Button component to HomePage

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import { Button } from "@/components/ui/button";
 import React from "react";
 
 const HomePage = () => {
   return (
     <div>
       <h1>HomePage</h1>
+      <Button>Click me</Button>
     </div>
   );
 };
