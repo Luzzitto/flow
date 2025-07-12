4a4387657a62a5ae56066f2206d9d42935c28a6d
Luzzitto Tupaz <36060968+Luzzitto@users.noreply.github.com>
2025-07-12 01:00:37 -0400

    Add ThemeProvider component and include next-themes dependency

---
+++ components/theme-provider.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { ThemeProvider as NextThemesProvider } from "next-themes";
+
+const ThemeProvider: React.FC<
+  React.ComponentProps<typeof NextThemesProvider>
+> = ({ children, ...props }) => {
+  return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
+};
+
+export default ThemeProvider;
