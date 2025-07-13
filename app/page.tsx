99aeeb2c6eda4281c08ee38de26f4dfc74ce65db
Luzzitto Tupaz <36060968+Luzzitto@users.noreply.github.com>
2025-07-13 01:45:38 -0400

    feat: add Card, Progress, Input, and Label components; update HomePage layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,48 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
-import { useTheme } from "next-themes";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Progress } from "@/components/ui/progress";
 import React from "react";
 
 const HomePage = () => {
-  const { theme } = useTheme();
   return (
-    <div>
-      <h1>HomePage</h1>
-      <Button>Click me</Button>
-      <p>Current theme: {theme}</p>
+    <div className="min-h-screen min-w-screen overflow-hidden flex flex-row">
+      <div className="basis-1/4 border"></div>
+      <div className="basis-3/4 border p-8">
+        <div className="grid grid-cols-4">
+          <Card>
+            <CardContent>
+              <CardTitle>FPV Drone</CardTitle>
+              <div className="flex items-center justify-center gap-4">
+                <div className="text-center">
+                  <div className="text-4xl font-bold tracking-tighter tabular-nums">
+                    $ 350
+                  </div>
+                  <div className="text-muted-foreground text-xs uppercase">
+                    / $1,000
+                  </div>
+                </div>
+              </div>
+              <div className="flex-1 pt-4">
+                <Progress value={33} />
+              </div>
+            </CardContent>
+            <CardFooter>
+              <Button variant="default" size="lg" className="w-full text">
+                Contribute
+              </Button>
+            </CardFooter>
+          </Card>
+        </div>
+      </div>
     </div>
   );
 };
